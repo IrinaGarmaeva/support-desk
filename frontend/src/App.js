d95d7d36ef268d4e635e2660c7935dcb6b855969
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-14 15:24:27 +0300

    Feature: UI for pages: Home, Register, Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Login from './pages/Login';
@@ -17,6 +19,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
+      <ToastContainer />
     </>
   );
 }
