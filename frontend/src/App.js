93d3f77bd261d990bf8e59e09bb99f7b9c4d1a94
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-14 14:41:58 +0300

    Feature: add Header and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      MY APP
-    </div>
+    <>
+      <BrowserRouter>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
