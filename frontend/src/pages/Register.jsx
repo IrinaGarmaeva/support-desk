d7e953f89766f428bc25ef74909a2c6d5ebaf02d
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-16 13:15:23 +0300

    Feature: hooking Up To Redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { toast } from 'react-toastify';
 import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch} from 'react-redux';
+import {register} from '../features/auth/authSlice';
 
 function Register() {
   const [formData, setFormdata] = useState({
@@ -11,6 +13,10 @@ function Register() {
   });
 
   const { name, email, password, passwordConfirmed } = formData;
+
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isSuccess, message } = useSelector((state) => state.auth)
 
   const onChange = (e) => {
     setFormdata((prevState) => ({
@@ -24,6 +30,12 @@ function Register() {
 
     if(password !== passwordConfirmed) {
       toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name, email, password
+      }
+
+      dispatch(register(userData))
     }
   }
 
