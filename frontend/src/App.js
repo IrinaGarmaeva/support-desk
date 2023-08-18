c753e0fb75dfd95f285b6cac4e51ce3ba830df63
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-18 20:32:15 +0300

    Feature Frontend: add protected route for new ticket page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </BrowserRouter>
